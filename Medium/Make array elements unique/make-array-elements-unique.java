e9158685e79b8e62103bd97c8a7d0573ea02ef76
Jatin_Bisht <82670244+JatinBisht2308@users.noreply.github.com>
2023-01-11 12:23:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Make array elements unique/make-array-elements-unique.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            
+            int i = 0;
+            int N = Integer.parseInt(br.readLine().trim());
+        
+            int arr[] = new int[(int)(N)];
+            
+            String inputLine2[] = br.readLine().trim().split(" ");
+            for ( i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(inputLine2[i]);
+            }
+        
+            Solution ob = new Solution();
+            System.out.println(ob.minIncrements(arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    public long minIncrements(int[] a, int n) {
+
+        long count=0;
+
+        Arrays.sort(a);
+
+        for(int i=1;i<n;i++)
+
+        {
+
+            if(a[i-1]>=a[i])
+
+            {
+
+                int temp=a[i-1]+1;
+
+                count+=temp-a[i];
+
+                a[i]=temp;
+
+            }
+
+        }
+
+        return count;
+
+    }
+
+}
